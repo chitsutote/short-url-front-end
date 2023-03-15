52472ef2ea9e638501d76167b75a90ae858b8d63
Wei-Cheng Ling <chitsutote7@gmail.com>
2023-03-15 20:53:36 +0800

    refactor: add return type to urlBuilder helper function

--- src/utils/urlBuilder.ts
+++ src/utils/urlBuilder.ts
@@ -1,5 +1,5 @@
 import config from '../config'
 
-export const shortUrlBuilder = (route: string) => `${config.shortUrlPrefix}/${route}`
+export const shortUrlBuilder = (route: string): string => `${config.shortUrlPrefix}/${route}`
 
-export const urlBuilder = (url: string) => url.indexOf('http://') >= 0 ? url : `http://${url}`
+export const urlBuilder = (url: string): string => url.indexOf('http://') >= 0 ? url : `http://${url}`
