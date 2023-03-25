8db6cd9d25a7a6840e1543462c65929b9b88ed30
Wei-Cheng Ling <chitsutote7@gmail.com>
2023-03-25 17:58:27 +0800

    fix: url that starts with https will be recognized as invalid url

--- src/utils/urlBuilder.ts
+++ src/utils/urlBuilder.ts
@@ -2,4 +2,4 @@ import config from '../config'
 
 export const shortUrlBuilder = (route: string): string => `${config.shortUrlPrefix}/${route}`
 
-export const urlBuilder = (url: string): string => url.indexOf('http://') >= 0 ? url : `http://${url}`
+export const urlBuilder = (url: string): string => /^http|^https/.test(url) ? url : `http://${url}`
